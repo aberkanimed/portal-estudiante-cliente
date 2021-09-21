fdb42268049d13965c04eee5a4d5f8c07a403a4e
Mohamed Aberkani <aberkanimed@gmail.com>
2021-09-21 10:15:15 +0200

    Register Implemented

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,25 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    user: null,
+    token: null,
   },
   mutations: {
+    SET_USER_DATA (state, data) {
+      state.user = data.usuario
+      localStorage.setItem('user', JSON.stringify(data.usuario))
+      localStorage.setItem('token', JSON.stringify(data.token))
+      apiClient.defaults.headers.common['Authorization'] = `Bearer ${ data.token }`
+    },
   },
   actions: {
+    register({ commit }, credentials) {
+      return apiClient
+        .post('/api/register', credentials)
+        .then(({ data }) => {
+          commit('SET_USER_DATA', data.data)
+        })
+    },
   },
   modules: {
   }
