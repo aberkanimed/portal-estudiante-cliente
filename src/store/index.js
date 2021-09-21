62c65d3b9f6bb9bfb0bce396fd3c6ef787326029
Mohamed Aberkani <aberkanimed@gmail.com>
2021-09-21 10:23:04 +0200

    Login Implemented

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,6 @@ import { createStore } from 'vuex'
 export default createStore({
   state: {
     user: null,
-    token: null,
   },
   mutations: {
     SET_USER_DATA (state, data) {
@@ -17,6 +16,13 @@ export default createStore({
     register({ commit }, credentials) {
       return apiClient
         .post('/api/register', credentials)
+        .then(({ data }) => {
+          commit('SET_USER_DATA', data.data)
+        })
+    },
+    login({ commit }, credentials) {
+      return apiClient
+        .post('/api/login', credentials)
         .then(({ data }) => {
           commit('SET_USER_DATA', data.data)
         })
